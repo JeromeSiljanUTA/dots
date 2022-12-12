eae7551de3ac7da0f9671853408a72d300a98b48
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2022-12-12 10:39:31 -0600

    feat(bspwm/scripts/color-change.sh): rudimentary color changer

---
+++ bspwm/scripts/color-change.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# purple
+accent_color="#B16286"
+#green
+#accent_color="#98971A"
+
+# dunst color change
+# change highlight color in dunst
+sed -i "s/highlight.*/highlight = \"$accent_color\"/" ~/.config/dunst/dunstrc
+# change frame color in dunst
+sed -i "s/frame.*/frame_color = \"$accent_color\"/" ~/.config/dunst/dunstrc
+
+killall dunst
+dunst &
+
+for proc in $(ps aux | grep "subscribe desktop" | awk '{print $2}')
+do 
+    kill $proc
+done
+
+bspc subscribe desktop | ~/.config/bspwm/scripts/desktop-notifier &
+ 
+# rofi color change
+# change accent color in rofi
+sed -i "0,/accent.*/{s/accent.*/accent:             $accent_color;/}" ~/.config/rofi/themes/gruvbox-green.rasi
+
+# tmux color change
+sed -i "s/status-style bg=.*/status-style bg=$accent_color/" ~/.config/tmux/tmux.conf
