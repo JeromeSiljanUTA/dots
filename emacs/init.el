e5eab200432e53c7c0ec59b6da88c1e12e9e5150
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2023-08-22 10:35:57 -0500

    feat(emacs/init.el): Commented codespaces configuration

--- emacs/init.el
+++ emacs/init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(calendar-mark-holidays-flag t)
  '(custom-safe-themes
-   '("ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3" "98ef36d4487bf5e816f89b1b1240d45755ec382c7029302f36ca6626faf44bbd" "19a2c0b92a6aa1580f1be2deb7b8a8e3a4857b6c6ccf522d00547878837267e7" "fa49766f2acb82e0097e7512ae4a1d6f4af4d6f4655a48170d0a00bcb7183970" "21055a064d6d673f666baaed35a69519841134829982cbbb76960575f43424db" "9ab9441566f7c3b059a205a7c5fad32a58422c2695815436d8cc087860b8c2e5" "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" default))
+   '("046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3" "98ef36d4487bf5e816f89b1b1240d45755ec382c7029302f36ca6626faf44bbd" "19a2c0b92a6aa1580f1be2deb7b8a8e3a4857b6c6ccf522d00547878837267e7" "fa49766f2acb82e0097e7512ae4a1d6f4af4d6f4655a48170d0a00bcb7183970" "21055a064d6d673f666baaed35a69519841134829982cbbb76960575f43424db" "9ab9441566f7c3b059a205a7c5fad32a58422c2695815436d8cc087860b8c2e5" "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" default))
  '(holiday-bahai-holidays nil)
  '(holiday-islamic-holidays nil)
  '(holiday-oriental-holidays nil)
@@ -143,12 +143,14 @@
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 
-;; https://github.com/patrickt/codespaces.el
-(use-package codespaces
-  :bind ("C-c S" . #'codespaces-connect)
-  :config
-  (codespaces-setup)
-  (setq vc-handled-backends '(Git)))
+
+;; Commented because I don't think I'll ever use this again
+;;;; https://github.com/patrickt/codespaces.el
+;;(use-package codespaces
+;;  :bind ("C-c S" . #'codespaces-connect)
+;;  :config
+;;  (codespaces-setup)
+;;  (setq vc-handled-backends '(Git)))
 
 
 
