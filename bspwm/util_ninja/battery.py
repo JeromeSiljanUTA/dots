163d607bf034abd2872a22d03bedf140b6c0cb1e
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2023-09-18 22:26:28 -0500

    feat(util_ninja): Initial commit

---
+++ bspwm/util_ninja/battery.py
@@ -0,0 +1,82 @@
+"""Get battery percentage, charging information, powerprofiles mode and push to
+notification.
+
+"""
+
+import os
+import subprocess
+
+BATTERY_PATH = " /sys/class/power_supply/BAT0/"
+os.environ["DISPLAY"] = ":0"
+
+
+def choose_battery_icon(battery_level: int) -> str:
+    # Choose a battery icon depending on the battery level.
+
+    if battery_level > 80:
+        battery_icon = "full"
+    elif battery_level > 50:
+        battery_icon = "good"
+    elif battery_level > 20:
+        battery_icon = "low"
+    else:
+        battery_icon = "caution"
+
+    return f"battery-{battery_icon}"
+
+
+def get_battery_info() -> tuple[int, str]:
+    """Parse acpi data for battery level and remaining time until battery
+    runs out.
+
+    """
+    battery_info = (
+        subprocess.run(["acpi", "b"], stdout=subprocess.PIPE, check=True)
+        .stdout.decode("utf-8")
+        .split("\n")
+    )
+
+    main_battery = [
+        battery for battery in battery_info if "unavailable" not in battery
+    ][0]
+
+    battery_level = int(main_battery.split(",")[1].split("%")[0])
+
+    try:
+        remaining_time = f", {main_battery.split(',')[2].strip()}"
+    except IndexError:
+        remaining_time = ""
+
+    return battery_level, remaining_time
+
+
+def get_power_profile() -> str:
+    # Parse powerprofiles for power mode.
+
+    result = subprocess.run(
+        "powerprofilesctl", stdout=subprocess.PIPE, check=True
+    ).stdout.decode("utf-8")
+    profile_line = result.split("*")[-1].split("\n")[0]
+
+    if "power-saver" in profile_line:
+        power_profile = "Power Saver"
+    elif "balanced" in profile_line:
+        power_profile = "Balanced"
+    else:
+        power_profile = "Performance"
+
+    return power_profile
+
+
+def send_battery_notification():
+    # Sends battery notification.
+    battery_level, remaining_time = get_battery_info()
+    power_profile = get_power_profile()
+    battery_icon = choose_battery_icon(battery_level)
+
+    message = (
+        f"'Battery Level' '{battery_level}% {remaining_time} \n"
+        f"Mode: {power_profile}' -i {battery_icon}"
+    )
+
+    os.system(f"dunstify -r 2 {message}")
