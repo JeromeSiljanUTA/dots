e46f667b8a4906d2388c0ad9274bc20aec98ee50
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2023-10-25 16:19:25 -0500

    feat(bspwm/scripts/desktop-notifier.sh): Display different icons for based on desktop names

--- bspwm/scripts/desktop-notifier.sh
+++ bspwm/scripts/desktop-notifier.sh
@@ -2,5 +2,31 @@
 
 bspc subscribe desktop_focus | while read _; do
     desktop_name="$(bspc query -D -d --names)"
-    dunstify -r 1 "$desktop_name" -i preferences-desktop-display
+    if [[ "$desktop_name" == "Terminal" ]]
+    then
+	dunstify -r 1 "$desktop_name" -i terminal-symbolic
+    elif [[ "$desktop_name" == "Notes" ]]
+    then
+	dunstify -r 1 "$desktop_name" -i notes-symbolic
+    elif [[ "$desktop_name" == "Emacs" ]]
+    then
+	dunstify -r 1 "$desktop_name" -i emacs-symbolic
+    elif [[ "$desktop_name" == "Firefox" ]]
+    then
+	dunstify -r 1 "$desktop_name" -i firefox-symbolic
+    elif [[ "$desktop_name" == "Comms" ]]
+    then
+	dunstify -r 1 "$desktop_name" -i messengerfordesktop-symbolic
+    elif [[ "$desktop_name" == "IDE" ]]
+    then
+	dunstify -r 1 "$desktop_name" -i programming-symbolic
+    elif [[ "$desktop_name" == "Files" ]]
+    then
+	dunstify -r 1 "$desktop_name" -i file-manager-symbolic
+    elif [[ "$desktop_name" == "Media" ]]
+    then
+	dunstify -r 1 "$desktop_name" -i library-music-symbolic
+    else
+	dunstify -r 1 "$desktop_name" -i preferences-desktop-display
+    fi
 done
