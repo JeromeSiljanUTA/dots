49a0c331113d192f0cbbe70d570d0bf4661e2daf
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2023-11-19 21:07:02 -0600

    feat(update): update

--- emacs/init.el
+++ emacs/init.el
@@ -7,6 +7,7 @@
  '(custom-safe-themes
    '("046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3" "98ef36d4487bf5e816f89b1b1240d45755ec382c7029302f36ca6626faf44bbd" "19a2c0b92a6aa1580f1be2deb7b8a8e3a4857b6c6ccf522d00547878837267e7" "fa49766f2acb82e0097e7512ae4a1d6f4af4d6f4655a48170d0a00bcb7183970" "21055a064d6d673f666baaed35a69519841134829982cbbb76960575f43424db" "9ab9441566f7c3b059a205a7c5fad32a58422c2695815436d8cc087860b8c2e5" "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" default))
  '(global-hl-line-mode t)
+ '(helm-move-to-line-cycle-in-source nil)
  '(holiday-bahai-holidays nil)
  '(holiday-islamic-holidays nil)
  '(holiday-oriental-holidays nil)
@@ -22,14 +23,20 @@
 		  ((org-agenda-overriding-header "Tasks @anywhere")))
        (tags-todo "@home"
 		  ((org-agenda-overriding-header "Tasks @home")))
+       (tags-todo "@couch"
+		  ((org-agenda-overriding-header "Tasks @couch")))
        (tags-todo "@waiting"
 		  ((org-agenda-overriding-header "Tasks @waiting"))))
       nil nil)))
+ '(org-agenda-files
+   '("~/learning/school/QUANTITATIVE COMPUTER ARCHITECTURE CSE4323/cse4323.org" "~/learning/school/EMBEDDED SYSTEMS II CSE4342/cse4342.org" "~/learning/school/SENIOR DESIGN II CSE4317/cse4317.org" "~/misc/gtd/main.org"))
+ '(org-agenda-loop-over-headlines-in-active-region nil)
+ '(org-babel-load-languages '((shell . t) (C . t)))
  '(org-edit-src-content-indentation 0)
  '(org-export-preserve-breaks nil)
  '(org-tags-column -50)
  '(package-selected-packages
-   '(wttrin olivetti package pandoc-mode dumb-jump arduino-mode docker-cli docker docker-compose-mode debian-el dockerfile-mode bluetooth pylint lua-mode pdf-tools eat jupyter codespaces ayu-theme nano-theme writeroom-mode helm numpydoc sphinx-doc avy flycheck format-all company yasnippet use-package reformatter python-mode magit expand-region evil emms ein direnv blacken auto-complete)))
+   '(org-roam wttrin olivetti package pandoc-mode dumb-jump arduino-mode docker-cli docker docker-compose-mode debian-el dockerfile-mode bluetooth pylint lua-mode pdf-tools eat jupyter codespaces ayu-theme nano-theme writeroom-mode helm numpydoc sphinx-doc avy flycheck format-all company yasnippet use-package reformatter python-mode magit expand-region evil emms ein direnv blacken auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -177,6 +184,17 @@
   :config
   (setq wttrin-default-cities '("Irving" "USA")))
 
+;; https://github.com/org-roam/org-roam
+(use-package org-roam)
+
+;; https://github.com/s-kostyaev/ellama
+(use-package ellama
+  :init
+  (setopt ellama-language "English")
+  (require 'llm-ollama)
+  (setopt ellama-provider
+	  (make-llm-ollama
+	   :chat-model "codellama" :embedding-model "codellama")))
 
 (require 'python)
 ;; C-c C-c respects __init__
@@ -188,14 +206,15 @@
 
 (set-frame-font "IBM Plex Mono 12" t)
 
-;; active Babel languages
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((shell . t)))
-
 ;; eshell colors
 (add-hook 'eshell-mode-hook (lambda () (setenv "TERM" "xterm-256color")))
 
+;; Start emacs daemon
+(server-start)
+
+;; Use ibuffer instead of BufferMenu
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 (provide 'init)
-(put 'dired-find-alternate-file 'disabled nil)
 ;;; init.el ends here
+(put 'dired-find-alternate-file 'disabled nil)
