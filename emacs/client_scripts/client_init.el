90054a58e39f47ceb53120e43d2a604180439cb4
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2023-11-22 00:24:06 -0600

    feat(emacs/client_scripts/): Initial commit

---
+++ emacs/client_scripts/client_init.el
@@ -0,0 +1,3 @@
+(set-frame-font "IBM Plex Mono 12")
+
+;;; client_init.el ends here
---
+++ emacs/client_scripts/light_theme.el
@@ -0,0 +1,6 @@
+(disable-theme 'gruvbox-dark-medium)
+(disable-theme 'gruvbox-light-medium)
+
+(load-theme 'gruvbox-light-medium)
+
+;;; client_init.el ends here
